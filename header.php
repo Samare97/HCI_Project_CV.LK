9ff0b6958708a17e7e675e680f48b56e6f0e2e9c
RushdyAhamed <rushdhyahamed98@gmail.com>
2020-09-06 19:38:40 +0530

    header edited

--- header.php
+++ header.php
@@ -14,6 +14,9 @@
       <li class="nav-item">
         <a class="nav-link" href="#">ALL Vacancies</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Post your vacancy</a>
+      </li>
 
       <li class="nav-item">
         <a class="nav-link" href="#">Contact Us</a>
