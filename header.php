4877dbf405ad2a3f9a6cfab4977994c1cc90be07
RushdyAhamed <rushdhyahamed98@gmail.com>
2020-10-01 23:36:56 +0530

    new_header

--- header.php
+++ header.php
@@ -1,23 +1,192 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="js/jquery-3.5.1.min.js"></script>
+
+<style>
+
+
+
+/*
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a, .dropbtn {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+  background-color: blue;
+}
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+*/
+
+
+.navbar2 {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar2 a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown2 {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown2 .dropbtn2 {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar2 a:hover, .dropdown2:hover .dropbtn2 {
+  background-color: blue;
+}
+
+.dropdown2-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown2-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown2-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown2:hover .dropdown2-content {
+  display: block;
+}
+
+.p3 {
+  color: blue;
+  font-size: 17px;
+}
+
+
+</style>
+</head>
+<body>
+<!--
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="jobseeker.php">Job Seekers</a></li>
+  <li><a href="All_vacancy.php">All Vacancy</a></li>
+  <li><a href="#">Foreign Vacancy</a>
+  <li><a href="vacancy_post.php">Post Vacancy</a></li>
+  <li><a href="#">Contact Us</a>
+</ul>
+
+-->
+
+<br>
+<div class="navbar2">
+  <div class="dropdown2">
+    <a href="#">Logo</a>
+    <a href="index.php">Home</a>
+    <a href="jobseeker.php">Job Seekers</a>
+    <a href="All_vacancy.php">All Vacancy</a>
+    <a href="#">Foreign Vacancy</a>
+    <a href="vacancy_post.php">Post Vacancy</a>
+    <a href="#">Contact Us</a>
+</div>
+  </div> 
+
+</body>
+</html>
+
+
+
+
+<!--
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <a class="navbar-brand" href="#">Company Logo</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home <span class="sr-only"> </span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Job Seekers</a>
+        <a class="nav-link" href="jobseeker.php">Job Seekers</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">ALL Vacancies</a>
+        <a class="nav-link" href="All_vacancy.php">All Vacancy</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Post your vacancy</a>
+        <a class="nav-link" href="#">Foreign Vacancy</a>
       </li>
-
+      <li class="nav-item">
+        <a class="nav-link" href="vacancy_post.php">Post Vacancy</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Contact Us</a>
       </li>
@@ -36,5 +205,6 @@
   </div>
 </nav>
 
+-->
 
 
